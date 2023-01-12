6b79fc76b515acfd4d516f658eed2999e772c0e3
Lukas Abelt <labelt@fc.ul.pt>
2023-01-12 10:35:04 +0000

    * Added test cases for state requirements and transitions of state layer

--- code/LayeredTypes/layer_implementations/state.py
+++ code/LayeredTypes/layer_implementations/state.py
@@ -4,91 +4,109 @@ from collections import defaultdict
 import lark.visitors
 
 
-def typecheck(tree):
-    class StateLayer(lark.visitors.Interpreter):
-        class ArgumentState:
-            def __init__(self):
-                self.required_states = set()
-                self.state_transitions = set()
+class ArgumentState:
+    def __init__(self):
+        self.required_states = set()
+        self.state_transitions = set()
 
-            def __init__(self, state: str):
-                STATES_REGEX = r"\{([a-zA-Z\s]*(=>[a-zA-Z\s]*)?(,[a-zA-Z\s]*(=>[a-zA-Z\s]*)?)*)\}"
-                if not re.match(STATES_REGEX, state):
-                    raise ValueError(f"Invalid state definition {state}")
+    def __init__(self, state: str):
+        self.required_states = set()
+        self.state_transitions = set()
 
-                state_parts = re.match(STATES_REGEX, state).group(1).split(",")
+        STATES_REGEX = r"\{([a-zA-Z\s\d]*(=>[a-zA-Z\s\d]*)?(,[a-zA-Z\s\d]*(=>[a-zA-Z\s\d]*)?)*)\}"
+        if not re.match(STATES_REGEX, state):
+            raise ValueError(f"Invalid state definition {state}")
 
-                for part in state_parts:
-                    if "=>" in part:
-                        before, after = [p.strip() for p in part.split("=>")]
-                        self.add_state_transition(before, after)
-                    else:
-                        self.required_states.add(part.strip())
+        state_parts = re.match(STATES_REGEX, state).group(1).split(",")
 
-            def add_required_state(self, state):
-                self.required_states.add(state)
-
-            def add_state_transition(self, before, after):
-                self.state_transitions.add((before, after))
-                self.required_states.add(before)
-
-            def check_required_states(self, states):
-                return self.required_states.issubset(states)
-
-            def apply_state_transitions(self, states):
-                for before, after in self.state_transitions:
-                    if not before or before in states:
-                        states.remove(before) if before in states else None
-                        states.add(after) if after else None
-        def __init__(self):
-            self.__states = defaultdict(set)
-            self.__function_states = dict()
-        def layer(self, tree):
-            identifier = tree.children[0].children[0].value
-            layer_name = tree.children[1].children[0].value
-
-            if layer_name != "state":
-                return tree
-
-            states = [t.strip for t in tree.children[2].value.split("->")]
-
-            if len(states) == 1:
-                # State definition for a identifier
-                # That case can be used to check that a variable has a specific state or manually define a transition
-                arg_state = self.ArgumentState(states[0])
-                if not arg_state.check_required_states(self.__states[identifier]):
-                    raise TypeError(f"{tree.meta.line}:{tree.meta.column}: Identifier {identifier} does not have the required state.")
-                arg_state.apply_state_transitions(self.__states[identifier])
-
+        for part in state_parts:
+            if "=>" in part:
+                before, after = [p.strip() for p in part.split("=>")]
+                self.add_state_transition(before, after)
             else:
-                #This is a definition for a function
-                self.__function_states[identifier] = [self.ArgumentState(s) for s in states]
+                self.required_states.add(part.strip()) if part.strip() else None
 
-        def fun_call(self, tree):
-            fun_identifier = tree.children[0].value
+    def add_required_state(self, state):
+        self.required_states.add(state)
 
-            if fun_identifier not in self.__function_states:
-                return set()
+    def add_state_transition(self, before, after):
+        self.state_transitions.add((before, after))
+        self.required_states.add(before)
 
-            # Check that each argument has the required states
-            for i, arg in enumerate(tree.children[1:-1]):
-                if not self.__function_states[fun_identifier][i].check_required_states(self.visit(arg)):
-                    raise TypeError(f"{tree.meta.line}:{tree.meta.column}: Function {fun_identifier} was called with invalid state for argument {i}.")
+    def check_required_states(self, states):
+        return self.required_states.issubset(states)
 
-            # Apply the state transitions
-            for i, arg in enumerate(tree.children[1:-1]):
-                self.__function_states[fun_identifier][i].apply_state_transitions(self.visit(arg))
+    def apply_state_transitions(self, states):
+        # We want to apply all transitions simultaneously
+        states_to_remove = set()
+        states_to_add = set()
 
-            # Return the states of the last argument which corresponds to the return value
-            return self.__function_states[fun_identifier][-1].required_states
+        for before, after in self.state_transitions:
+            if before in states or not before:
+                states_to_remove.add(before) if before else None
+                states_to_add.add(after) if after else None
 
-        def ident(self, tree):
-            return self.__states[tree.children[0].value] if tree.value in self.__states else set()
+        states.difference_update(states_to_remove)
+        states.update(states_to_add)
 
-        def assign(self, tree):
-            self.__states[tree.children[0].value] = self.visit(tree.children[1])
 
-        def fun_def(self, tree):
-            raise NotImplementedError()
-        def __default__(self, tree):
+class StateLayer(lark.visitors.Interpreter):
+    def __init__(self):
+        self.__states = defaultdict(set)
+        self.__function_states = dict()
+
+    def layer(self, tree):
+        identifier = tree.children[0].children[0].value
+        layer_name = tree.children[1].children[0].value
+
+        if layer_name != "state":
+            return tree
+
+        states = [t.strip for t in tree.children[2].value.split("->")]
+
+        if len(states) == 1:
+            # State definition for a identifier
+            # That case can be used to check that a variable has a specific state or manually define a transition
+            arg_state = self.ArgumentState(states[0])
+            if not arg_state.check_required_states(self.__states[identifier]):
+                raise TypeError(
+                    f"{tree.meta.line}:{tree.meta.column}: Identifier {identifier} does not have the required state.")
+            arg_state.apply_state_transitions(self.__states[identifier])
+
+        else:
+            # This is a definition for a function
+            self.__function_states[identifier] = [self.ArgumentState(s) for s in states]
+
+    def fun_call(self, tree):
+        fun_identifier = tree.children[0].value
+
+        if fun_identifier not in self.__function_states:
             return set()
+
+        # Check that each argument has the required states
+        for i, arg in enumerate(tree.children[1:-1]):
+            if not self.__function_states[fun_identifier][i].check_required_states(self.visit(arg)):
+                raise TypeError(
+                    f"{tree.meta.line}:{tree.meta.column}: Function {fun_identifier} was called with invalid state for argument {i}.")
+
+        # Apply the state transitions
+        for i, arg in enumerate(tree.children[1:-1]):
+            self.__function_states[fun_identifier][i].apply_state_transitions(self.visit(arg))
+
+        # Return the states of the last argument which corresponds to the return value
+        return self.__function_states[fun_identifier][-1].required_states
+
+    def ident(self, tree):
+        return self.__states[tree.children[0].value] if tree.value in self.__states else set()
+
+    def assign(self, tree):
+        self.__states[tree.children[0].value] = self.visit(tree.children[1])
+
+    def fun_def(self, tree):
+        raise NotImplementedError()
+
+    def __default__(self, tree):
+        return set()
+
+def typecheck(tree):
+    pass
---
+++ code/LayeredTypes/tests/test_state_layer.py
@@ -0,0 +1,130 @@
+import unittest
+
+from layer_implementations.state import ArgumentState
+
+class StateLayer(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+class ArgumentStateParsing(unittest.TestCase):
+    def assertEmpty(self, l):
+        self.assertEqual(len(l), 0)
+    def test_parse_empty(self):
+        arg_state = ArgumentState("{}")
+        self.assertEmpty(arg_state.required_states)
+        self.assertEmpty(arg_state.state_transitions)
+
+    def test_parse_single_state_requirement(self):
+        arg_state = ArgumentState("{state}")
+        self.assertSetEqual(arg_state.required_states, {"state"})
+        self.assertEmpty(arg_state.state_transitions)
+
+    def test_parse_single_state_transition(self):
+        arg_state = ArgumentState("{state => otherState}")
+        self.assertSetEqual(arg_state.required_states, {"state"})
+        self.assertSetEqual(arg_state.state_transitions, {("state", "otherState")})
+
+    def test_parse_multiple_state_transitions(self):
+        arg_state = ArgumentState("{state1 => state2, state3 => state4}")
+        self.assertSetEqual(arg_state.required_states, {"state1", "state3"})
+        self.assertSetEqual(arg_state.state_transitions, {("state1", "state2"), ("state3", "state4")})
+
+    def test_parse_multiple_state_requirements(self):
+        arg_state = ArgumentState("{state1, state2}")
+        self.assertSetEqual(arg_state.required_states, {"state1", "state2"})
+        self.assertEmpty(arg_state.state_transitions)
+
+    def test_parse_multiple_state_requirements_and_transitions(self):
+        arg_state = ArgumentState("{state1, state2 => state3, state4 => state5}")
+        self.assertSetEqual(arg_state.required_states, {"state1", "state2", "state4"})
+        self.assertSetEqual(arg_state.state_transitions, {("state2", "state3"), ("state4", "state5")})
+
+    def test_invalid_format(self):
+        with self.assertRaises(ValueError):
+            ArgumentState("{state1 => state2, state3 => state4")
+
+    def test_invalid_format2(self):
+        with self.assertRaises(ValueError):
+            ArgumentState("state1 => state2, state3 => state4")
+
+class ArgumentStateLogic(unittest.TestCase):
+    def test_check_required_states(self):
+        arg_state = ArgumentState("{state1, state2 => state3, state4 => state5}")
+        self.assertTrue(arg_state.check_required_states({"state1", "state2", "state4"}))
+        self.assertFalse(arg_state.check_required_states({"state1"}))
+        self.assertFalse(arg_state.check_required_states({"state2"}))
+        self.assertFalse(arg_state.check_required_states({"state4"}))
+        self.assertFalse(arg_state.check_required_states({"state1", "state2"}))
+        self.assertFalse(arg_state.check_required_states({"state1", "state4"}))
+        self.assertFalse(arg_state.check_required_states({"state2", "state4"}))
+
+    def test_apply_state_transitions(self):
+        arg_state = ArgumentState("{state1, state2 => state3, state4 => state5}")
+        states = {"state1", "state2", "state4"}
+        arg_state.apply_state_transitions(states)
+        self.assertSetEqual(states, {"state1","state3", "state5"})
+
+    def test_apply_state_transitions2(self):
+        arg_state = ArgumentState("{state1, state2 => state3, state4 => state5}")
+        states = {"state1", "state2", "state3", "state4"}
+        arg_state.apply_state_transitions(states)
+        self.assertSetEqual(states, {"state1","state3", "state5"})
+
+    def test_apply_transition_remove_and_create(self):
+        arg_state = ArgumentState("{state1 => state2, state2 => state3}")
+        states = {"state1", "state2"}
+        arg_state.apply_state_transitions(states)
+        self.assertSetEqual(states, {"state2","state3"})
+
+    def test_apply_transition_remove_and_create2(self):
+        arg_state = ArgumentState("{state1 => state2, state2 => state3}")
+        states = {"state1", "state2", "state3"}
+        arg_state.apply_state_transitions(states)
+        self.assertSetEqual(states, {"state2","state3"})
+
+    def test_apply_transition_remove_and_create3(self):
+        arg_state = ArgumentState("{state3 => state1, state1 => state2}")
+        states = {"state3", "state1"}
+        arg_state.apply_state_transitions(states)
+        self.assertSetEqual(states, {"state2","state1"})
+
+    def test_apply_transition_remove_and_create4(self):
+        arg_state = ArgumentState("{a=>b, b=>c, c=>d, d=>e}")
+        states = {"a", "b", "c", "d"}
+        arg_state.apply_state_transitions(states)
+        self.assertSetEqual(states, {"b","c","d","e"})
+
+    def test_apply_empty_transition(self):
+        arg_state = ArgumentState("{state1 =>}")
+        states = {"state1", "state2"}
+        arg_state.apply_state_transitions(states)
+        self.assertSetEqual(states, {"state2"})
+
+    def test_apply_empty_transition2(self):
+        arg_state = ArgumentState("{state1 =>}")
+        states = {"state1"}
+        arg_state.apply_state_transitions(states)
+        self.assertSetEqual(states, set())
+
+    def test_apply_empty_transition3(self):
+        arg_state = ArgumentState("{state1 =>, state2 =>}")
+        states = {"state2", "state1"}
+        arg_state.apply_state_transitions(states)
+        self.assertSetEqual(states, set())
+
+    def test_state_transition_create(self):
+        arg_state = ArgumentState("{ => state2}")
+        states = set()
+        arg_state.apply_state_transitions(states)
+        self.assertSetEqual(states, {"state2"})
+
+    def test_state_transition_create2(self):
+        arg_state = ArgumentState("{ => state2, state1 => state3}")
+        states = {"state1"}
+        arg_state.apply_state_transitions(states)
+        self.assertSetEqual(states, {"state2", "state3"})
+
+
+
+if __name__ == '__main__':
+    unittest.main()
