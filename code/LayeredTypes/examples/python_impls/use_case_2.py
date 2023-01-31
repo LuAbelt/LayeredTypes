af63dd1c8281a187133f21aa142d22a3b34b0b05
Lukas Abelt <labelt@fc.ul.pt>
2023-01-31 13:22:22 +0000

    * Added bubble sort implementation for use case 2

---
+++ code/LayeredTypes/examples/python_impls/use_case_2.py
@@ -0,0 +1,11 @@
+from builtins import len, print
+
+def set_element(ls, idx, val):
+    ls[idx] = val
+
+def get_element(ls, idx):
+    return ls[idx]
+
+def get_list():
+    # Return some unordered list with 12 elements
+    return [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
