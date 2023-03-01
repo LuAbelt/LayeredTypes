cfff7b412ff71dd5e5295faec616a1fa20fe3813
Lukas Abelt <labelt@fc.ul.pt>
2023-03-01 15:16:05 +0000

    * Added test cases to cover more behavior of liquid types

--- code/LayeredTypes/tests/test_liquid.py
+++ code/LayeredTypes/tests/test_liquid.py
@@ -17,14 +17,43 @@ class TestLiquidLayer(unittest.TestCase):
         with self.assertRaises(Exception):
             compiler.typecheck(src_file)
 
+    def test_simple_assignment_infer_fail(self):
+        compiler = get_compiler()
+        src_file = full_path("/test_code/liquid/simple_assignment_infer_fail.fl")
+
+        with self.assertRaises(Exception):
+            compiler.typecheck(src_file)
+
     def test_fun_call_assign(self):
-        typecheck_correct_file(self, "/test_code/liquid/fun_call_assign.fl")
+        typecheck_correct_file(self, "test_code/liquid/fun_call_oneArg_assign.fl")
 
     def test_fun_call_noArgs(self):
         typecheck_correct_file(self, "/test_code/liquid/fun_call_noArgs.fl")
 
+    def test_fun_call_noArgs_assign(self):
+        typecheck_correct_file(self, "/test_code/liquid/fun_call_noArgs_assign.fl")
+
     def test_fun_call_oneArg(self):
         typecheck_correct_file(self, "/test_code/liquid/fun_call_oneArg.fl")
 
+    def test_fun_call_oneArg_assign(self):
+        typecheck_correct_file(self, "/test_code/liquid/fun_call_oneArg_assign.fl")
+
     def test_fun_call_twoArgs(self):
         typecheck_correct_file(self, "/test_code/liquid/fun_call_twoArgs.fl")
+
+    def test_fun_call_twoArgs_assign(self):
+        typecheck_correct_file(self, "/test_code/liquid/fun_call_twoArgs_assign.fl")
+
+    def test_nested_fun_call(self):
+        typecheck_correct_file(self, "/test_code/liquid/nested_fun_call.fl")
+
+    def test_let_inner_layer(self):
+        typecheck_correct_file(self, "/test_code/liquid/let_inner_layer.fl")
+
+    def test_let_inner_layer_fail(self):
+        compiler = get_compiler()
+        src_file = full_path("/test_code/liquid/let_inner_layer_fail.fl")
+
+        with self.assertRaises(Exception):
+            compiler.typecheck(src_file)
