20bf634f63d0e33e87891c3863c0cde863453919
Lukas Abelt <labelt@fc.ul.pt>
2023-03-16 11:58:21 +0000

    * Updated use case 2 to only use pure functions and do not re-assign variables

--- code/LayeredTypes/examples/python_impls/use_case_2.py
+++ code/LayeredTypes/examples/python_impls/use_case_2.py
@@ -1,11 +1,13 @@
+from random import randrange
 from builtins import len, print
 
-def set_element(ls, idx, val):
+def set_list_element(ls, idx, val):
     ls[idx] = val
+    return ls
 
 def get_element(ls, idx):
     return ls[idx]
 
 def get_list():
-    # Return some unordered list with 12 elements
-    return [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    # Return list with 12 random elements
+    return [randrange( 100) for _ in range(12)]
